b77dd26a5ea89fbb7a58c0e54b4347082db23553
AhnYeonghoo <dksdudgn00@naver.com>
2022-08-17 18:51:19 +0900

    private SECRET-KEY hide

---
+++ README.md
@@ -0,0 +1,17 @@
+# django_self_study
+
+장고로 내 블로그 제작
+
+<br>
+
+# Stack
+
+- Python 3.10
+- Javascript
+- Html
+- Css
+- Django 3.1.3
+- MariaDB
+- Docker
+- Git(Version Control)
+- OpenSource
---
+++ private/urls.py
@@ -0,0 +1,12 @@
+"""private URL Configuration
+
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('account/', include('accountapp.urls')),
+]
